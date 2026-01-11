ed30e6780b17a7cdc2cd2aa7342ffbc7081d302c
Evie Finch <eviefinch@Evies-MacBook-Air.local>
2026-01-11 16:56:51 -0600

    cms stuff

--- _cms.ts
+++ _cms.ts
@@ -2,6 +2,15 @@ import lumeCMS from "lume/cms/mod.ts";
 
 const cms = lumeCMS();
 
+const user = Deno.env.get("CMS_USER") ?? "admin";
+const pass = Deno.env.get("CMS_PASSWORD") ?? "";
+
+cms.auth({
+  [user]: pass,
+});
+
+cms.git();
+
 cms.collection("posts", "src:posts/*.md", [
   "title: text",
   "content: markdown",
